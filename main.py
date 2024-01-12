e1a6c663cf5eea9d2fbc11002ec8b86765249ef6
LuckyCurve <50801099+LuckyCurve@users.noreply.github.com>
2024-01-12 10:54:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def get_weather_and_push_message():
 
 
 if __name__ == '__main__':
+    get_weather_and_push_message()
     scheduler = BlockingScheduler()
     # 添加定时任务，每天的 10 点和晚上 8 点执行
     scheduler.add_job(get_weather_and_push_message, 'cron', hour='10,20')
